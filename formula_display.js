b66590d498b52184513730040899f690da5813ff
Micrafast <1191069521@qq.com>
2022-04-08 19:59:00 +0800

    update display

--- formula_display.js
+++ formula_display.js
@@ -1,5 +1,5 @@
 function convertToTex(chemicalFormulaStr) {
-    let t1 = chemicalFormulaStr.replace(/([A-Z][a-z]*)(\d*)/g, "{$1}_{$2}")
+    let t1 = chemicalFormulaStr.replace(/([A-Z][a-z]*)(\d+)/g, "{$1}_{$2}")
     let t2 = t1.replace(/\^(\d*)([-*])/g, "^{$1$2}").replace(/\*/g, "+")
     return t2.replace(/\)(\d+)/g, ")_{$1}")
 }
